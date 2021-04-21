441b140ac704959756622feaefd0caf0d762caa0
sergeyi7 <poleshhuk97@gmail.com>
2021-04-21 10:51:23 +0600

    Первый рабочий ответ бота

--- base.py
+++ base.py
@@ -1,1 +1,10 @@
 import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=['text', 'start', 'help'])
+def text_message(message):
+    bot.send_message(message.from_user.id, 'Hello World!')
+
+bot.polling()#(none_stop=True, interval=0)
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 name = 'probabot'
 username = 'Zaika_proba_bot'
 TOKEN = '1734479949:AAGYhnxAMQmp0S1qEKtXS55Fd2yP2wBDEq8'
-documentation_for_telegram_bot_api = 'https://core.telegram.org/bots/api'
+documentation_for_telegram_bot_api = 'https://core.telegram.org/bots/api  '
