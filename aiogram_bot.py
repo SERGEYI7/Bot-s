97e92ee46f6ac1ce92dc0918852d9aaba73c608c
ZAIKA <poleshhuk97@gmail.com>
2021-04-23 01:31:13 +0600

    trying to add a keyboard

--- aiogram_bot.py
+++ aiogram_bot.py
@@ -1,24 +1,40 @@
 import logging
+import aiogram.kb as kb
 from config import TOKEN
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardMarkup
 
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+btn_hi = KeyboardButton('Я кнопка bth_hi')
+btn_atr = ReplyKeyboardMarkup()
+btn_atr.add(btn_atr)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome_start(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply("Привет, этот бот скоро порвёт твоё очко.", reply_markup=kb.btn_atr)
+
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Скоро здесь будут интересные команды.")
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
 
-    await message.answer('Привет, мля!!!')
+    await message.answer('Ассам алеёкууум!!!!!!!!!!!!!!!')
 
 executor.start_polling(dp, skip_updates=True)
